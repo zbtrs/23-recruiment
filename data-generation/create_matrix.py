bdf1d68227428b93ee47b4a099a1ce779414d35d
Wenda Zhang <vaaandark@gmail.com>
2023-05-16 11:44:11 +0800

    add test data generation

---
+++ data-generation/create_matrix.py
@@ -0,0 +1,83 @@
+"""
+Usage:
+  > import create_matrix
+  > import random
+  > mat_size = random.choice(create_matrix.SPM_SIZES)
+  > mat_type = random.choice(create_matrix.SPARSE_MATRIX_TYPES)
+  > mat = create_matrix.create_sparse_matrix(mat_type, *mat_size) # 生成稀疏矩阵
+  > create_matrix.dump_to_file(mat, "matrix.bin") # 写入二进制文件
+  > mat = create_matrix.read_from_file("matrix.bin") # 读入二进制文件
+"""
+
+import random
+import struct
+
+DENSITY = random.uniform(0, 0.5)
+UPPER, LOWER = 100, -100
+SPM_SIZES = [(256, 256), (512, 512), (1024, 1024)]
+
+def _generate_random_float():
+  return random.uniform(UPPER, LOWER)
+
+# 对称稀疏矩阵
+def _symmetric_matrix(m, n):
+  chance = DENSITY / 2
+  mat = [[_generate_random_float() if j >= i and random.random() < chance
+          else .0 for j in range(n)]
+          for i in range(m)]
+  for i in range(m):
+    for j in range(n):
+      if mat[i][j] != .0:
+        mat[j][i] = mat[i][j]
+  return mat
+
+# （上）三角稀疏矩阵
+def _triangular_matrix(m, n):
+  chance = DENSITY * 2
+  return [[_generate_random_float() if j >= i and random.random() < chance
+          else .0 for j in range(n)]
+          for i in range(m)]
+
+# 对角稀疏矩阵
+def _diagonal_matrix(m, n):
+  return [[_generate_random_float() if i == j else .0 for j in range(n)]
+          for i in range(m)]
+
+# 条带系数矩阵
+def _banded_matrix(m, n):
+  half_width = random.randint(1, min(m, n) // 3)
+  return [[_generate_random_float() if abs(i - j) <= half_width else .0 for j in range(n)]
+          for i in range(m)]
+
+# 一般稀疏矩阵
+def _general_matrix(m, n):
+  return [[_generate_random_float() if random.random() < DENSITY
+          else .0 for _ in range(n)]
+          for _ in range(m)]
+
+SPARSE_MATRIX_TYPES = [_symmetric_matrix, _triangular_matrix, _diagonal_matrix, _banded_matrix, _general_matrix]
+
+def _check_sparsity(mat):
+  total = len(mat) * len(mat[0])
+  zores = sum(row.count(.0) for row in mat)
+  return zores / total > 0.5
+
+def create_sparse_matrix(func, m, n) -> list[list[float]]:
+  while True:
+    mat = func(m, n)
+    if _check_sparsity(mat):
+      return mat
+
+def dump_to_file(mat, filename):
+  m, n = len(mat), len(mat[0])
+  with open(filename, "wb") as f:
+    f.write(struct.pack("i", m))
+    f.write(struct.pack("i", n))
+    for row in mat:
+      for num in row:
+        f.write(struct.pack("f", num))
+
+def read_from_file(filename) -> list[list[float]]:
+  with open(filename, "rb") as f:
+    m, n = struct.unpack("ii", f.read(8))
+    return [[struct.unpack('f', f.read(4))[0] for _ in range(n)] for _ in range(m)]
