db73c6793abf4c7c67898fdbd2da4b4676edb648
Wenda Zhang <vaaandark@gmail.com>
2023-05-16 11:46:59 +0800

    support importing data from binary files and modify SparseMatrix class

--- SparseMatrix.cc
+++ SparseMatrix.cc
@@ -1,8 +1,11 @@
 #include "SparseMatrix.hh"
 
 #include <random>
+#include <fstream>
+#include <cassert>
+#include <tuple>
 
-SparseMatrix::SparseMatrix(int m, int n) {
+SparseMatrix::SparseMatrix(int m, int n): _m(m), _n(n) {
   static default_random_engine e;
   bernoulli_distribution b(.01);
   uniform_real_distribution<float> u(0, 1);
@@ -17,5 +20,26 @@ SparseMatrix::SparseMatrix(int m, int n) {
 }
 
 SparseMatrix::SparseMatrix(string &filename) {
+  ifstream in(filename, ios::in | ios::binary);
+  assert(in);
+  int m, n;
+  in.read(reinterpret_cast<char *>(&m), sizeof(int));
+  in.read(reinterpret_cast<char *>(&n), sizeof(int));
+  _m = m;
+  _n = n;
+  vector<float> row;
+  for (int i = 0; i < m; ++i) {
+    row.clear();
+    for (int j = 0; j < n; ++j) {
+      float num;
+      in.read(reinterpret_cast<char *>(&num), sizeof(float));
+      row.push_back(num);
+    }
+    _matrix.push_back(row);
+  }
+  in.close();
+}
 
+tuple<int, int> SparseMatrix::size() {
+  return make_tuple(_m, _n);
 }
--- SparseMatrix.hh
+++ SparseMatrix.hh
@@ -1,19 +1,21 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <tuple>
 using namespace std;
 
 struct SparseMatrix {
   vector<vector<float>> _matrix;
+  int _m, _n;
 
   SparseMatrix() = default;
   // generate random data
   SparseMatrix(int m, int n);
   // import data from file
   SparseMatrix(string &filename);
-  SparseMatrix(const vector<vector<float>> &m) : _matrix(m) {}
+  SparseMatrix(const vector<vector<float>> &m) : _matrix(m), _m(m.size()), _n(m[0].size()) {}
   
-  
+  tuple<int, int> size();
 };
 
 vector<float> operator*(const vector<float> &vec, const SparseMatrix &matrix);
